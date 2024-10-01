50bdcaa32bb0bae8a697364b059be0f41d0b4c15
suyeon54 <waterkite54@gmail.com>
2024-10-01 11:14:22 +0900

    etude_update2
    etude_update2

--- script/action.js
+++ script/action.js
@@ -1,10 +1,10 @@
 let options = 0;
 
-$('.gnb li').mouseover(function(){
+/* $('.gnb li').mouseover(function(){
     if(options == 0){
         $('.lnb').removeClass('on')
         $('.lnb').stop().slideUp(300)
-        $(this).find('.lnb').stop().slideDown(300);
+        $('.lnb').stop().slideDown(300);
     }
 })
 
@@ -12,11 +12,10 @@ $('.gnb li').mouseout(function(){
     if(options == 0){
         $('.lnb').stop().slideUp(300)
     }
-})
+}) */
 
 let lnbMaxH = 0
 $('.mo_menu').click(function(){
-    $('.lnb').addClass('on') //배경지우기
     $('.lnb').stop().slideToggle(300, function(){
         $('.lnb').each(function(){
             let lnbH = $(this).outerHeight()  //높이3개
@@ -34,6 +33,15 @@ $('.mo_menu').click(function(){
         })
     });
 
+})
+
+//pc menu
+$('header .gnb').mouseenter(function(){
+    $('.lnb').stop().slideDown(300);
+})
+
+$('header .gnb').mouseleave(function(){
+    $('.lnb').stop().slideUp(300);
 })
 
 if(document.cookie.indexOf('dark') > -1){
@@ -57,7 +65,7 @@ $('.sun').click(function(){
     document.cookie = 'mode = light';
 })
 
-let recentTop = parseInt($('.recent_prod').css('top'));
+/* let recentTop = parseInt($('.recent_prod').css('top'));
 $(window).scroll(function(){
     let scrollTop = $(window).scrollTop();
     console.log(scrollTop)
@@ -67,8 +75,28 @@ $(window).scroll(function(){
         $('.recent_prod').stop().animate({top: '50%'})
     }
     
-})
+}) */
 
 $('.gotop').click(function(){
     $('html, body').animate({scrollTop:0})
+})
+
+$('.on2').click(function(){
+        $('.detail_img').hide()
+        $('.text_box').show()
+})
+
+$('.on').click(function(){
+    $('.detail_img').show()
+    $('.text_box').hide()
+})
+
+$('.login_box ul input').keyup(function(){
+    let id = $(this).val()
+    console.log(id);
+    if(id != ''){
+        $('.login_box button').addClass('on')
+    } else {
+        $('.login_box button').removeClass('on')
+    }
 })
